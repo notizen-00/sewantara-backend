98cab168b082083a86f6e56942c054db3cadd8c3
Notizen__ <dipta.ikromi@unej.ac.id>
2026-07-31 12:27:11 +0700

    feat(broadcasting):add reverb package

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,3 +14,11 @@ searchInput?.addEventListener('input', (event) => {
         row.hidden = query.length > 0 && !row.textContent.toLowerCase().includes(query);
     });
 });
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allow your team to quickly build robust real-time web applications.
+ */
+
+import './echo';
